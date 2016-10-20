12dadccc7dc286017d7c24fc3867dc954af1809b
blue <bluedai180@gmail.com>
2016-10-20 17:31:08 +0800

    Solve No.7~10 questions.

--- DatMiningExercise.py
+++ DatMiningExercise.py
@@ -39,12 +39,43 @@ data = open("twitter.txt", 'r', encoding='utf-8')
 # data.close()
 
 # 6.该文本里，输出在每一天发表tweets最多的用户。（要求：输出一个字典。例如 {'2012-03-04':'agelin','2012-03-5':'twa'}）
-result_dict = dict()
-for x in data.readlines():
-    result.append((x.split('","')[6].split(" ")[0], x.split('","')[2]))
-result_day = [(x, result.count(x)) for x in set(result)]
-result_day.sort(key=lambda k: (k[0][0], k[1]))
-for y in result_day:
-    result_dict[y[0][0]] = y[0][1]
-print(result_dict)
-data.close()
+# result_dict = dict()
+# for x in data.readlines():
+#     result.append((x.split('","')[6].split(" ")[0], x.split('","')[2]))
+# result_day = [(x, result.count(x)) for x in set(result)]
+# result_day.sort(key=lambda k: (k[0][0], k[1]))
+# for y in result_day:
+#     result_dict[y[0][0]] = y[0][1]
+# print(result_dict)
+# data.close()
+
+# 7. 请按照时间顺序输出 2012-11-03 每个小时的发布tweets的频率（要求：输出为一个list [(1,20),(2,30)] 代表1点发了20个tweets，2点发了30个tweets）
+# for x in data.readlines():
+#     if x.split('","')[6].split(" ")[0] == "2012-11-03":
+#         result.append(time.strptime(x.split('","')[6], '%Y-%m-%d %H:%M:%S').tm_hour)
+# result_hour = [(y, result.count(y)) for y in result]
+# print(set(result_hour))
+# data.close()
+
+# 8. 统计该文本里，来源的相关信息和次数，比如（输出一个list。例如[('Twitter for Android',1),('TweetList!',1)]）
+# for x in data.readlines():
+#     result.append(x.split('","')[7])
+# result_source = [(y, result.count(y)) for y in result]
+# print(set(result_source))
+# data.close()
+
+# 9. 计算转发URL中：以："https://twitter.com/umiushi_no_uta"开头的有几个。(要求，输出一个整数。)
+# for x in data.readlines():
+#     if "https://twitter.com/umiushi_no_uta" in x.split('","')[34]:
+#         result.append(x.split('","')[34])
+# print(len(result))
+# data.close()
+
+# 10. UID为573638104的用户 发了多少个微博 （要求：输出一个整数）
+# for x in data.readlines():
+#     if x.split('","')[1] == "573638104":
+#         result.append(x.split('","')[1])
+# print(len(result))
+# data.close()
+
+# 11. 定义一个函数，该函数可放入任意多的用户uid参数（如果不存在则返回null），函数返回发微薄数最多的用户uid。
