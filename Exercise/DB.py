4ca8d43d8e6ec243d9812bb313a8e7a21ad781ea
bluedai180 <bluedai180@gmail.com>
2016-12-13 23:17:40 +0800

    Add DB exercise.

---
+++ Exercise/DB.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+conn = mysql.connector.connect(user='root', password='blue', database='test')
+cursor = conn.cursor()
+
+cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+cursor.execute('insert into user (id, name) values (%s, %s)', ['1', 'Dai'])
+print(cursor.rowcount)
+conn.commit()
+cursor.close()
+cursor = conn.cursor()
+cursor.execute('select * from user where id = %s', ('1',))
+values = cursor.fetchall()
+print(values)
+cursor.close()
+conn.close()
