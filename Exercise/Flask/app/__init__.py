1997b53dc100db46b0d4e7ad9770312d99fc0ca0
bluedai180 <bluedai180@gmail.com>
2017-01-05 22:57:17 +0800

    Change architecture for Flasky

---
+++ Exercise/Flask/app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, config
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    return app
