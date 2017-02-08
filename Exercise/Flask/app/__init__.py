21a59f8568bf261e7ac11b74faa285a5ea1fba43
blue <bluedai180@gmail.com>
2017-02-08 19:19:40 +0800

    Change files to fit complicated web app.

--- Exercise/Flask/app/__init__.py
+++ Exercise/Flask/app/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, config
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from config import config
 
 
 bootstrap = Bootstrap()
--- Exercise/Flask/config.py
+++ Exercise/Flask/config.py
@@ -4,6 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dai'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = ''
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
