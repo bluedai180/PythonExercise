b5b20b5107b55f8edb895e1fd3b60fbea4ee1292
blue <bluedai180@gmail.com>
2017-02-10 14:43:35 +0800

    Add Flasky login func.

--- Exercise/Flask/app/main/views.py
+++ Exercise/Flask/app/main/views.py
@@ -1,24 +1,7 @@
-from datetime import datetime
-from flask import render_template, session, redirect, url_for
+from flask import render_template
 
 from . import main
-from .forms import NameForm
-from .. import db
-from ..models import User
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('.index'))
-    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False),
-                           current_time=datetime.utcnow())
+    return render_template('index.html')
