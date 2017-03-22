2c44c269f7f08b3774e75d50fc90882a60ec1b85
bluedai180 <bluedai180@gmail.com>
2017-03-23 00:40:30 +0800

    Update Combination.py

--- Exercise/Combination.py
+++ Exercise/Combination.py
@@ -1,20 +1,42 @@
 import itertools
 
-num = [1, 4, 2, 3, 5]
+num = [9, 5, 8, 4, 9, 4, 10]
+num_dict = {'0': 9, '1': 5, '2': 8, '3': 4, '4': 9, '5': 4, '6': 10}
 length = len(num)
 index = []
 num_sort = sorted(num)
+# print([i for i,v in enumerate(num) if v == 5])
+# print(num[1:].index(9))
 
-for i in range(length):
-    num_total = list(itertools.combinations(num_sort, length - i))
-    for x in num_total:
-        for y in list(x):
-            index.append(str(num.index(y)))
-        if "".join(index) == "".join(sorted("".join(index))):
-            print(x)
-            print(len(x))
-            exit()
-        index.clear()
+# for i in range(length):
+#     num_total = list(itertools.combinations(num_sort, length - i))
+#     for x in num_total:
+#         for y in list(x):
+#             if ()
+#             index.append(str(num.index(y)))
+#         if "".join(index) == "".join(sorted("".join(index))):
+#             print(x)
+#             print(len(x))
+#             exit()
+#         index.clear()
+
+num_total = list(itertools.combinations(num_sort, 4))
+for x in num_total:
+    print(x)
+    for y in list(x):
+        temp = [i for i, v in enumerate(num) if v == y]
+        if (len(temp) > 1):
+            # for z in temp:
+            #     index.append(str(z))
+            pass
+        else:
+            index.append(str(temp[0]))
+
+    if "".join(index) == "".join(sorted("".join(index))):
+        print(x)
+        print(len(x))
+        exit()
+    index.clear()
 
 
 
