c0503464e9f455bda1ad80c7aefba11ccd79d74f
Blue <bluedai180@gmail.com>
2017-04-19 23:55:40 +0800

    Update log.py, add wrong message control.

--- Exercise/log.py
+++ Exercise/log.py
@@ -24,35 +24,44 @@ class MonkeyLog(object):
     def get_info(self, app_name, message, index):
         name = []
         detail = []
-        info_index = []
-        info_message = []
-        info_total_message = []
-        f = open(self.log_path, 'r', encoding='utf-8')
-        for line, data in enumerate(f):
-            if data.startswith(app_name):
-                name.append(data.split(' ')[2].replace('\n', ''))
-                info_index = range(line + 6, line + 50)
-                if len(info_message) != 0:
-                    info_total_message.append("".join(info_message))
-                    info_message.clear()
-                continue
-            if data.startswith(message):
-                detail.append(' '.join(data.split(' ')[index:]).replace('\n', ''))
-                continue
-            if line in info_index:
-                if data.startswith('//'):
-                    info_message.append(data)
-        info_total_message.append("".join(info_message))
+        lines_name = []
+        lines_message = []
+        wrong_lines = []
+        i = 0
+        with open(self.log_path, 'r', encoding='utf-8') as data:
+            for x in data:
+                i += 1
+                if x.startswith(app_name):
+                    name.append(x.split(' ')[2].replace('\n', ''))
+                    lines_name.append(i)
+                if x.startswith(message):
+                    detail.append(' '.join(x.split(' ')[index:]).replace('\n', ''))
+                    lines_message.append(i)
+
+        for x in lines_message:
+            if x - 2 not in lines_name:
+                detail.remove(detail[lines_message.index(x)])
+                wrong_lines.append(str(x))
+
+        for x in lines_name:
+            if x + 2 not in lines_message:
+                name.remove(name[lines_name.index(x)])
+                wrong_lines.append(str(x))
 
         if len(name) != len(detail):
             print("Wrong message !!!!!!!")
-        if len(name) != len(info_total_message):
-            print("Wrong message")
-
         for i in range(len(name)):
-            temp = [name[i], detail[i], info_total_message[i]]
+            temp = [name[i], detail[i]]
             self.info_total.append(temp)
-        f.close()
+
+        print("========================================")
+        if len(wrong_lines) != 0:
+            if app_name == "// CRASH: ":
+                print("Crash数据中，log文件格式错误的行数：")
+                print('\n'.join(wrong_lines))
+            else:
+                print("ANR数据中，log文件格式错误的行数：")
+                print('\n'.join(wrong_lines))
 
     def analyze_info(self):
         self.info_total.sort(key=lambda x: x[0])
@@ -66,13 +75,9 @@ class MonkeyLog(object):
             if x not in self.info_final:
                 self.info_final.append(x)
 
-
     def write_excel(self, ws, title_style, content_style, content_long_style):
-        ws.append(['总数', '进程名', '错误信息', '详细信息', '复现次数'])
+        ws.append(['总数', '进程名', '错误信息', '复现次数'])
         merge_line_num = self.count_merge(self.info_final)
-        merge_line_bug_num = self.count_merge_bug(self.info_final)
-        print(merge_line_bug_num)
-        print(merge_line_num)
         length = len(merge_line_num)
         for i in range(length):
             if length == 1:
@@ -82,15 +87,15 @@ class MonkeyLog(object):
                 if merge_line_num[i + 1] - merge_line_num[i] != 1:
                     ws.merge_cells('A%s:A%s' % (merge_line_num[i] + 1, merge_line_num[i + 1]))
                     ws.merge_cells('B%s:B%s' % (merge_line_num[i] + 1, merge_line_num[i + 1]))
-                if merge_line_bug_num[i + 1] - merge_line_bug_num[i] != 1:
-                    ws.merge_cells('C%s:C%s' % (merge_line_bug_num[i] + 1, merge_line_bug_num[i + 1]))
         for x in self.info_final:
             ws.append(x)
         self.format_excel(ws, title_style, content_style, content_long_style)
         wb.save(os.path.dirname(os.path.abspath(self.log_path)) + "\log.xlsx")
         self.info_final.clear()
         self.info_total.clear()
+
         print("%s表数据已经写入." % ws.title)
+        print("========================================\n")
 
     def count_app(self, info):
         temp_name = []
@@ -118,25 +123,13 @@ class MonkeyLog(object):
                 temp_num.append(length + 1)
         return temp_num
 
-    def count_merge_bug(self, info):
-        temp_num = []
-        length = len(info)
-        for i in range(length):
-            if i <length - 1:
-                if info[i][2] != info[i+1][2]:
-                    temp_num.append(i + 2)
-            else:
-                temp_num.append(length + 1)
-        return temp_num
-
     def format_excel(self, ws, title_style, content_style, content_long_style):
         ws.column_dimensions['A'].width = 8
         ws.column_dimensions['B'].width = 30
-        ws.column_dimensions['C'].width = 80
-        ws.column_dimensions['D'].width = 100
-        ws.column_dimensions['E'].width = 10
+        ws.column_dimensions['C'].width = 100
+        ws.column_dimensions['D'].width = 10
         for i in range(ws.max_row):
-            ws.row_dimensions[i + 1].height = 60
+            ws.row_dimensions[i + 1].height = 30
         for x in ws[1]:
             x.style = title_style
         for x in ws['A:B']:
@@ -145,8 +138,6 @@ class MonkeyLog(object):
         for x in ws['C'][1:]:
             x.style = content_long_style
         for x in ws['D'][1:]:
-            x.style = content_long_style
-        for x in ws['E'][1:]:
             x.style = content_style
 
     @staticmethod
@@ -197,10 +188,10 @@ if __name__ == '__main__':
     ws_crash.title = "crash"
     crash.write_excel(ws_crash, title, content, content_long)
 
-    # anr.get_info('ANR in', 'Reason:', 1)
-    # anr.analyze_info()
-    # ws_anr = wb.create_sheet("anr")
-    # anr.write_excel(ws_anr, title, content, content_long)
+    anr.get_info('ANR in', 'Reason:', 1)
+    anr.analyze_info()
+    ws_anr = wb.create_sheet("anr")
+    anr.write_excel(ws_anr, title, content, content_long)
     print("数据已经导出完毕，请到其log文件目录下查看log.xlsx文件！")
 
 
