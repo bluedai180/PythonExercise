40319a7f2111e1e09a9fdb46167adc8e8119ceea
blue <bluedai180@gmail.com>
2017-06-29 18:37:14 +0800

    Add wechat.py

---
+++ Exercise/wechat_itchat.py
@@ -0,0 +1,13 @@
+# import itchat
+#
+# itchat.auto_login()
+#
+# itchat.send('Hello, filehelper', toUserName='刘迎军')
+from wxpy import *
+import time
+# bot = Bot(qr_path="E:\\test.png")
+# my_friend = bot.friends().search('刘迎军')[0]
+# while True:
+#     time.sleep(5)
+#     my_friend.send('Hello WeChat!')
+# embed()
