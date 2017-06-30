ad55644011fb93efd87919b3b0218464471c7e57
blue <bluedai180@gmail.com>
2017-06-30 11:46:09 +0800

    update wechat, add  alarm clock.

--- Exercise/wechat_itchat.py
+++ Exercise/wechat_itchat.py
@@ -2,17 +2,22 @@ from wxpy import *
 import requests
 from bs4 import BeautifulSoup
 import json
+import time
+from apscheduler.schedulers.blocking import BlockingScheduler
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
 }
 key = "a5d11204de6b4869a9a8a107f9f4390c"
 
-info = ["*********【自动消息】*********"]
+info = []
+
 
 def get_date():
     response = requests.get("http://www.baibaidu.com/", headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
     info.append(soup.find("h3").a.string)
+
 
 def get_traffic_control():
     response = requests.get("http://xianxing.911cha.com/beijing.html", headers=headers, timeout=5)
@@ -24,27 +29,44 @@ def get_traffic_control():
             info.append("今日限行尾号：%s和%s" % (str(x).split(">")[1][0], str(x).split(">")[3][1]))
             break
 
+
 def get_weather():
-    response = requests.get("https://free-api.heweather.com/v5/weather?city=beijing&key=a5d11204de6b4869a9a8a107f9f4390c", headers=headers)
+    response = requests.get(
+        "https://free-api.heweather.com/v5/weather?city=beijing&key=a5d11204de6b4869a9a8a107f9f4390c", headers=headers)
     json_info = json.loads(response.text)
-    aqi = "PM2.5浓度%s，空气质量%s" % (json_info['HeWeather5'][0]['aqi']['city']['pm25'], json_info['HeWeather5'][0]['aqi']['city']['qlty'])
+    aqi = "PM2.5浓度%s，空气质量%s" % (
+        json_info['HeWeather5'][0]['aqi']['city']['pm25'], json_info['HeWeather5'][0]['aqi']['city']['qlty'])
     cond_d = "白天%s" % json_info['HeWeather5'][0]['daily_forecast'][0]['cond']['txt_d']
     cond_n = "夜间%s" % json_info['HeWeather5'][0]['daily_forecast'][0]['cond']['txt_n']
     hum = "湿度%s%%" % json_info['HeWeather5'][0]['daily_forecast'][0]['hum']
-    tmp = "最高温度%s℃，最低温度%s℃" % (json_info['HeWeather5'][0]['daily_forecast'][0]['tmp']['max'], json_info['HeWeather5'][0]['daily_forecast'][0]['tmp']['min'])
+    tmp = "最高温度%s℃，最低温度%s℃" % (json_info['HeWeather5'][0]['daily_forecast'][0]['tmp']['max'],
+                               json_info['HeWeather5'][0]['daily_forecast'][0]['tmp']['min'])
     vis = "能见度%skm" % json_info['HeWeather5'][0]['daily_forecast'][0]['vis']
-    wind = "%s%s" % (json_info['HeWeather5'][0]['daily_forecast'][0]['wind']['dir'],json_info['HeWeather5'][0]['daily_forecast'][0]['wind']['sc'])
+    wind = "%s，%s" % (json_info['HeWeather5'][0]['daily_forecast'][0]['wind']['dir'],
+                      json_info['HeWeather5'][0]['daily_forecast'][0]['wind']['sc'])
     info.append("%s，%s，%s，%s，%s，%s，%s" % (cond_d, cond_n, tmp, wind, aqi, hum, vis))
 
+
 def send_wechat(message):
-    bot = Bot(qr_path="E:\\test.png")
-    my_friend = bot.groups().search(keywords="骚星战队")[0]
+    my_friend = bot.groups().search(keywords="东坝老司机们")[0]
     my_friend.send(message)
 
-if __name__ == "__main__":
+
+def job():
+    info.append("*********【自动消息】*********")
     get_date()
     get_weather()
     get_traffic_control()
-    info.append("************************")
-    # send_wechat("\n\n".join(info))
+    send_wechat("\n\n".join(info))
     print("\n\n".join(info))
+    info.clear()
+
+
+if __name__ == "__main__":
+    bot = Bot(qr_path="E:\\test.png")
+    scheduler = BlockingScheduler()
+    scheduler.add_job(job, 'cron', day_of_week='1-7', hour=6, minute=00)
+    scheduler.start()
+    # while True:
+    #     time.sleep(5)
+    #     job()
